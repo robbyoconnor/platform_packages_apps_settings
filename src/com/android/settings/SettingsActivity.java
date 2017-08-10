voidz777-pure/android_packages_apps_Settings

Add back blacklist settings. [Squashed]
Cm originally had this squashed, but with the mess that is cm-13 even theyre squashed became unsquashed and you had to dig in like 10 commits that had nothing to do with blacklist to find all the code. thats what happens when one person merges fwbase and another setting.. c'mon man organize your shit!

src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.nexus.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
@@ -364,7 +365,8 @@ public class SettingsActivity extends Activity
             BatteryLightSettings.class.getName(),
             NotificationLightSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            BlacklistSettings.class.getName()
     };
 
 

robbyoconnor/platform_packages_apps_settings

src/com/android/settings/SettingsActivity.java
@@ -80,6 +80,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.nexus.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
@@ -365,9 +366,10 @@ public class SettingsActivity extends Activity
             WriteSettingsDetails.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             DisplayRotation.class.getName(),
+            BlacklistSettings.class.getName(),
+            HeadsUpSettings.class.getName(),
             PureSettings.class.getName(),
-            AmbientSettings.class.getName(),
-            HeadsUpSettings.class.getName()
+            AmbientSettings.class.getName()
     };
 
 